88fc07c3968ac3811865acb039167c3e7dcb08e3
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-10-01 20:44:46 +0200

    Serve /Scripts/WebSharper and /Content/WebSharper

--- WebSharper.Suave/Main.fs
+++ WebSharper.Suave/Main.fs
@@ -2,8 +2,11 @@ namespace WebSharper.Suave
 
 open global.Owin
 open System.Net
+open System.IO
 open Suave
 open Suave.Types
+open Suave.Http
+open Suave.Http.Applicatives
 open Suave.Http.Successful
 open Suave.Web
 open Suave.Owin
@@ -12,12 +15,20 @@ open WebSharper.Owin
 type WebSharperAdapter =
 
     static member ToWebPart(app, ?RootDirectory: string, ?BinDirectory: string) =
-        let rootDirectory = defaultArg RootDirectory ""
+        let rootDirectory =
+            match RootDirectory with
+            | Some d -> d
+            | None -> "../.."
+        let rootDirectory = DirectoryInfo(rootDirectory).FullName
         let binDirectory =
             match BinDirectory with
             | Some d -> d
             | None ->
                 typeof<WebSharperAdapter>.Assembly.Location
-                |> System.IO.Path.GetDirectoryName
-        SiteletMiddleware<_>.AsMidFunc(Options.Create(rootDirectory, binDirectory), app)
-        |> Suave.Owin.OwinApp.ofMidFunc
+                |> Path.GetDirectoryName
+        choose [
+            pathScan "/Scripts/WebSharper/%s" (Files.browseFile (Path.Combine(rootDirectory, "Scripts", "WebSharper")))
+            pathScan "/Content/WebSharper/%s" (Files.browseFile (Path.Combine(rootDirectory, "Content", "WebSharper")))
+            SiteletMiddleware<_>.AsMidFunc(Options.Create(rootDirectory, binDirectory), app)
+            |> Suave.Owin.OwinApp.ofMidFunc
+        ]
